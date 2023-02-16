66beb37a6ad43ec1d6382d2e61cd2cc52372909a
Javier Serrano <javier.serrano@tii.ae>
2023-02-16 11:25:05 +0000

    server with sweepers

--- rfsocServer.py
+++ rfsocServer.py
@@ -4,11 +4,11 @@ import numpy as np
 #from pynq import Overlay
 
 import time
-from qick import AveragerProgram, QickSoc
+from qick import AveragerProgram, QickSoc, RAveragerProgram
 import json
 ######################################################
 #
-class Program(AveragerProgram):
+class ProgramExecute(AveragerProgram):
 #
 ######################################################
 
@@ -18,63 +18,61 @@ class Program(AveragerProgram):
         self.soc = soc
         self.cfg = cfg
         self.max_gain = self.cfg["max_gain"]
+        #ro_duration = self.cfg["MZ"]["duration"]* 0.001
+        #ro_amplitude = self.cfg["MZ"]["amplitude"]
 
 
         # HW configuration
-        self.ro_channel = self.cfg["res_ch"] 
-        self.qd_channel = self.cfg["qubit_ch"]
+        self.ro_channel = 0 #self.cfg["ports"]["o0"]["channel"] 
+        self.qd_channel = 1 #self.cfg["ports"]["o1"]["channel"] 
+        self.ro_input_channel = 0 #self.cfg["ports"]["i0"]["channel"] 
 
         # Readout configuration
-        self.cfg["f_res"] = self.cfg["resonator_freq"] * MU
-        self.ro_freq = self.cfg["f_res"]
-        self.ro_length = self.soc.us2cycles(self.cfg["readout_length"]* 0.001 , gen_ch=self.ro_channel) # conver pulse lengths to clock ticks
+        self.ro_freq = self.cfg["ro_frequency"]* MU
+        ro_duration = self.cfg["ro_duration"]* 0.001
+        #ro_amplitude = self.cfg["MZ"]["amplitude"]  
+        #self.ro_gain = int( ro_amplitude * self.max_gain)      
+        self.ro_length = self.soc.us2cycles(ro_duration , gen_ch=self.ro_channel) # conver pulse lengths to clock ticks
         self.res_phase = self.cfg["resonator_phase"]
         self.add_trig_offset = self.cfg["adc_trig_offset"]
-        self.ro_gain = int(self.cfg["resonator_gain"] * self.max_gain)
-        self.cfg["res_gain"] = self.ro_gain
 
         # Qubit configuration
-        self.cfg["f_ge"]= self.cfg["qubit_freq"] * MU
-        self.qd_freq = self.cfg["f_ge"]
-        self.qd_length = self.soc.us2cycles(4*self.cfg["sigma"]* 0.001 , gen_ch=self.qd_channel)    # conver pulse lengths to clock ticks    
-        self.qd_gain = self.cfg["pi_gain"] * self.max_gain
+        #self.qd_freq = self.cfg["RX"]["frequency"] * MU
+
 
         # Experiment
-        self.cfg["reps"] = self.cfg["hardware_avg"]
+        cfg["reps"] = self.cfg["hardware_avg"]
         self.relax_delay = self.cfg["relax_delay"]
-
-
-
-
+        self.delay_before_readout = 0
         super().__init__(soc, cfg)
  
 
     def initialize(self):
+
         MU = 0.000001
-        self.cfg["f_res"] = self.cfg["resonator_freq"] * MU
-        self.cfg["f_ge"]= self.cfg["qubit_freq"] * MU
-        self.qd_freq = self.cfg["f_ge"]
-        self.ro_freq = self.cfg["f_res"]
+
         self.declare_gen(ch=self.ro_channel, nqz=2) # Readout
         self.declare_gen(ch=self.qd_channel, nqz=2) # Qubit
         self.drive_pulses = False
         # configure the readout lengths and downconversion frequencies
-        for ch in [0, 1]:
-            self.declare_readout(ch=ch, length=self.ro_length, freq= self.ro_freq , gen_ch=self.ro_channel)
+        #for ch in [0, 1]:
+        #    self.declare_readout(ch=ch, length=self.ro_length, freq= self.ro_freq , gen_ch=self.ro_channel)
+        self.declare_readout(ch=self.ro_input_channel, length=self.ro_length, freq= self.ro_freq , gen_ch=self.ro_channel)
 
         # add qubit and readout pulses to respective channels
 
-        # convert frequencies to dac register value  
-        self.qd_frequency = self.soc.freq2reg(self.qd_freq, gen_ch=self.qd_channel)
-        self.ro_frequency = self.soc.freq2reg(self.ro_freq  , gen_ch=self.ro_channel, ro_ch=0)  
-        #print("Readout freq: ",self.ro_freq , "Readout Gain: ", self.ro_gain)
-        print(self.sequence)
+    
+        #self.ro_frequency = self.soc.freq2reg(self.ro_freq  , gen_ch=self.ro_channel, ro_ch=0)  
+    
         for i, pulse in enumerate(self.sequence):
             p = self.sequence[pulse]
-            if p["channel"] == 1:
+            if p["type"] == 'qd':
                 self.drive_pulses = True  
-                sigma = self.soc.us2cycles(p["rel_sigma"]*0.001, gen_ch=1)
+                self.qd_gain = int(p["amplitude"] * self.max_gain)
                 duration = self.soc.us2cycles(p["duration"]*0.001, gen_ch=1)  
+                sigma = self.soc.us2cycles(p["duration"]*0.001/p["rel_sigma"], gen_ch=1)
+                # convert frequencies to dac register value  
+                self.qd_frequency = self.soc.freq2reg(p["frequency"], gen_ch=self.qd_channel)
                 if p['shape'] == "Gaussian":   
                     self.add_gauss(ch=self.qd_channel, name="Gaussian", sigma=sigma, length=duration )
                     
@@ -82,57 +80,143 @@ class Program(AveragerProgram):
                         ch=self.qd_channel,
                         style="arb",
                         freq= self.qd_frequency ,
-                        phase= int(p["relative_phase"]),
-                        gain= int(p["amplitude"] * self.max_gain),
+                        phase= self.deg2reg(self.res_phase, gen_ch=self.qd_channel), #int(p["relative_phase"]),
+                        gain= self.qd_gain,
                         waveform="Gaussian"
                         )
 
                 elif p['shape'] == "Drag":          
                     self.add_DRAG(ch=self.qd_channel, name="Drag", sigma=sigma, 
-                        length=duration, delta=20.0, alpha = p["beta"] )
+                        length=duration, delta=sigma, alpha = p["beta"] )
                     
                     self.set_pulse_registers(
                         ch=self.qd_channel,
                         style="arb",
                         freq= self.qd_frequency ,
                         phase= int(p["relative_phase"]),
-                        gain= int(p["amplitude"] * self.max_gain),
+                        gain= self.qd_gain,
                         waveform="Drag"
-                        )        
-                print("Drive Pulse: ",i,  p)   
+                        )       
+                print("amplitude, frecuencia drive, duracion y sigma: ",self.qd_gain, self.qd_frequency,  duration, sigma, self.qd_gain)   
 
-            elif p["channel"] == 0:
-                freq=self.freq2reg(self.ro_freq, gen_ch=self.ro_channel, ro_ch=0) 
+            elif p["type"] == 'ro':
+                freq=self.freq2reg(p["frequency"], gen_ch=self.ro_channel, ro_ch=0)
+                self.delay_before_readout = self.soc.us2cycles(p["start"]*0.001)
                 self.set_pulse_registers(
                     ch=self.ro_channel,
                     style="const",
-                    freq=self.ro_frequency,                  
+                    freq=freq,                  
                     phase= int(p["relative_phase"]), 
-                    gain= int(self.ro_gain),
+                    gain= int(p["amplitude"]*self.max_gain),
                     length= self.ro_length
                     )
-                print("Readout Pulse: ",i,  p)   
+
      
         
         self.synci(200)
 
     def body(self):
 
-        delay_before_readout = self.soc.us2cycles(0.05)
+        
 
         # play drive pulse
         if self.drive_pulses:
             self.pulse(ch=self.qd_channel)
         # align channels and wait some time (defined in the runcard)
-        self.sync_all(delay_before_readout)
-
+        self.sync_all(self.delay_before_readout)
+        print("Check point 1 - delay_before_readout", self.delay_before_readout)
         # trigger measurement, play measurement pulse, wait for qubit to relax
 
         self.measure(pulse_ch=self.ro_channel, 
-            adcs=[0, 1], 
+            #adcs=[0, 1], 
+            adcs=[0],
             adc_trig_offset = self.add_trig_offset,
             wait=True, 
             syncdelay=self.us2cycles(self.cfg["relax_delay"]))
+
+
+######################################################
+#
+class ProgramSweepFreq(RAveragerProgram):
+#
+######################################################
+    def __init__(self, soc, cfg, sequence, range): 
+
+ 
+        MHz = 0.000001  
+        mu_s = 0.001
+        self.soc = soc
+        self.cfg = cfg
+        self.max_gain = self.cfg["max_gain"]
+        self.start = range['start']
+        self.step = range['step']        
+        self.expt = range['expt']
+        # HW configuration
+        self.ro_channel = 0 
+        self.qd_channel = 1 
+        self.ro_input_channel = 0 
+
+        # Readout configuration
+        self.ro_freq = self.cfg["ro_frequency"]* MHz
+        ro_duration = self.cfg["ro_duration"]* mu_s     
+        self.ro_length = self.soc.us2cycles(ro_duration , gen_ch=self.ro_channel) # conver pulse lengths to clock ticks
+        self.res_phase = self.cfg["resonator_phase"]
+        self.add_trig_offset = self.cfg["adc_trig_offset"]
+        self.ro_gain = self.cfg['ro_gain']
+
+        # Qubit configuration
+        self.qubit_gain = sequence['0']['amplitude'] * self.max_gain   #self.cfg['qubit_gain']  # TODO chage to the pulse amplitude
+        self.probe_length = sequence['0']['duration'] * mu_s   #self.cfg['probe_length']  # TODO chage to the pulse duration
+
+        print(f'Check point 354: {self.probe_length}')  
+
+        # Experiment
+        cfg["reps"] = self.cfg["hardware_avg"]
+        cfg['expts'] = self.expt
+        self.relax_delay = self.cfg["relax_delay"]
+        self.delay_before_readout = 0
+        super().__init__(soc, cfg)
+
+    def initialize(self):
+        #cfg=self.cfg
+        
+        self.declare_gen(ch=self.ro_channel, nqz=2) #Readout
+        self.declare_gen(ch=self.qd_channel, nqz=2) #Qubit
+
+        self.declare_readout(ch=self.ro_input_channel, length=self.ro_length, freq= self.ro_freq , gen_ch=self.ro_channel)
+
+
+        self.q_rp=self.ch_page(self.qd_channel)     # get register page for qubit_ch
+        self.r_freq=self.sreg(self.qd_channel, "freq")   # get frequency register for qd_ch    
+        
+        f_res=self.freq2reg(self.ro_freq, gen_ch=self.ro_channel, ro_ch=self.ro_input_channel) # conver ro_freq to dac register value
+
+        self.f_start =self.freq2reg(self.start, gen_ch= self.qd_channel)  # get start/step frequencies
+        self.f_step =self.freq2reg(self.step, gen_ch=self.qd_channel)
+
+        # add qubit and readout pulses to respective channels
+        self.set_pulse_registers(ch=self.qd_channel, style="const", freq=self.f_start, phase=0, gain=self.qubit_gain, 
+                                 length=  self.probe_length)
+        self.set_pulse_registers(ch=self.ro_channel, style="const", freq=f_res, phase=self.res_phase, gain=self.ro_gain, 
+                                 length=self.ro_length)
+        
+        self.sync_all(self.us2cycles(1))
+    
+    def body(self):
+        self.pulse(ch=self.qd_channel)  #play probe pulse
+        self.sync_all(self.us2cycles(0.05)) # align channels and wait 50ns
+
+        #trigger measurement, play measurement pulse, wait for qubit to relax
+        self.measure(pulse_ch=self.ro_channel, 
+             adcs=[self.ro_input_channel],
+             adc_trig_offset=self.add_trig_offset,
+             wait=True,
+             syncdelay=self.us2cycles(self.cfg["relax_delay"]))
+
+    def update(self): 
+        self.mathi(self.q_rp, self.r_freq, self.r_freq, '+', self.f_step) # update frequency list index
+ 
+
 
 ###################################################
 #
@@ -162,17 +246,35 @@ class MyTCPHandler(BaseRequestHandler):
         jsonReceived = self.request.recv(2048)
 
         data = json.loads(jsonReceived.decode('utf-8'))
-
         if data['opCode'] == "setup":
+
             MyTCPHandler.cfg = data     
         elif data['opCode'] == "execute":
             del data['opCode']
-            sequence = data
-            program = Program(MyTCPHandler.soc, MyTCPHandler.cfg, sequence)
+            sequence = data  
+            program = ProgramExecute(MyTCPHandler.soc, MyTCPHandler.cfg, sequence)
             avgi, avgq = program.acquire(MyTCPHandler.soc, load_pulses=True, progress=False, debug=False)
             jsonDic = {"avgi": avgi[0][0], "avgq": avgq[0][0]}
             self.request.sendall(json.dumps(jsonDic).encode())
-        
+        elif data['opCode'] == "sweep":
+ 
+            del data['opCode']
+            parameter = data.pop('parameter')  
+            if parameter == "Parameter.frequency":               
+                program = ProgramSweepFreq(MyTCPHandler.soc, MyTCPHandler.cfg, data['pulses'], data['range'])
+                expt_pts, avg_di, avg_dq = program.acquire(MyTCPHandler.soc,load_pulses=True,progress=False, debug=False)
+                print(f'Check point 5: {expt_pts}')
+            elif parameter == 'Parameter.amplitude':
+                ProgramSweepAmp(MyTCPHandler.soc, MyTCPHandler.cfg, data.values)
+            else:
+                sequence = data['pulses']
+                program = ProgramExecute(MyTCPHandler.soc, MyTCPHandler.cfg, sequence)
+                avgi, avgq = program.acquire(MyTCPHandler.soc, load_pulses=True, progress=False, debug=False)
+
+            #jsonDic =  {"avgi": avgi[0][0], "avgq": avgq[0][0]}
+            jsonDic = {"avgi": -10.0, "avgq": 58.3}
+            self.request.sendall(json.dumps(jsonDic).encode())
+                
         else:
             print('Doing nothing')   
 
