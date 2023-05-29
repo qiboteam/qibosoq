2abbcb5c579909a663d31ae0831a7dc2675923a4
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-05-29 13:16:43 +0400

    test

--- tests/test_qick_programs.py
+++ tests/test_qick_programs.py
@@ -30,13 +30,25 @@ def test_execute_pulse_sequence__init__(soc):
             start=0,
             duration=0.04,
             name="pulse0",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse1",
             type="readout",
             dac=6,
             adc=0,
             shape="rectangular",
-        )
+        ),
     ]
-    qubits = [Qubit(0, 0)]
+    qubits = [Qubit()]
 
     program = ExecutePulseSequence(soc, config, sequence, qubits)
     assert isinstance(program, ExecutePulseSequence)
