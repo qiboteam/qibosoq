8718e2ed556ed2f710c3217542f91aece231bb26
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-05-29 13:23:57 +0400

    test

--- tests/test_qick_programs.py
+++ tests/test_qick_programs.py
@@ -4,7 +4,7 @@ import qick
 qick.QickSoc = None
 
 import qibosoq.configuration
-from qibosoq.components import Config, Pulse, Qubit
+from qibosoq.components import Config, Parameter, Pulse, Qubit, Sweeper
 from qibosoq.qick_programs import ExecutePulseSequence, ExecuteSweeps
 
 qibosoq.configuration.IS_MULTIPLEXED = False
@@ -52,3 +52,39 @@ def test_execute_pulse_sequence__init__(soc):
 
     program = ExecutePulseSequence(soc, config, sequence, qubits)
     assert isinstance(program, ExecutePulseSequence)
+
+
+def test_execute_sweeps__init__(soc):
+    config = Config()
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse0",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse1",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+    ]
+    sweepers = [Sweeper(expts=1000, parameter=[Parameter.FREQUENCY], starts=[0], stops=[100], indexes=[0])]
+
+    qubits = [Qubit()]
+
+    program = ExecuteSweeps(soc, config, sequence, qubits, sweepers)
+    assert isinstance(program, ExecuteSweeps)
