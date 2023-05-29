fc46af60f83fcaa0557d10613cd9bf81fd959332
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-05-29 13:08:43 +0400

    test

--- tests/test_qick_programs.py
+++ tests/test_qick_programs.py
@@ -1,17 +1,42 @@
+import pytest
 import qick
 
 qick.QickSoc = None
 
-from qibosoq.abstracts import Config, Pulse, Qubit
+import qibosoq.configuration
+from qibosoq.components import Config, Pulse, Qubit
 from qibosoq.qick_programs import ExecutePulseSequence, ExecuteSweeps
 
+qibosoq.configuration.IS_MULTIPLEXED = False
 
-def test_execute_pulse_sequence():
-    soc = {"us2cycles": 0}
+
+@pytest.fixture
+def soc():
+    def mock():
+        pass
+
     soc = qick.QickConfig("tests/qick_config.json")
+    soc.reset_gens = mock
+    return soc
+
+
+def test_execute_pulse_sequence__init__(soc):
     config = Config()
-    sequence = []
-    qubits = []
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse0",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        )
+    ]
+    qubits = [Qubit(0, 0)]
 
     program = ExecutePulseSequence(soc, config, sequence, qubits)
     assert isinstance(program, ExecutePulseSequence)
