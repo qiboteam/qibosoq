035f48a67eccc2ec2380c7984c1bc79635b462e1
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-05-30 16:03:18 +0400

    add tests

--- tests/test_qick_programs.py
+++ tests/test_qick_programs.py
@@ -22,7 +22,8 @@ def soc():
     return soc
 
 
-def test_execute_pulse_sequence__init__(soc):
+@pytest.fixture
+def execute_pulse_sequence(soc):
     config = Config()
     sequence = [
         Pulse(
@@ -53,10 +54,11 @@ def test_execute_pulse_sequence__init__(soc):
     qubits = [Qubit()]
 
     program = ExecutePulseSequence(soc, config, sequence, qubits)
-    assert isinstance(program, ExecutePulseSequence)
+    return program
 
 
-def test_execute_sweeps__init__(soc):
+@pytest.fixture
+def execute_sweeps(soc):
     config = Config()
     sequence = [
         Pulse(
@@ -89,4 +91,234 @@ def test_execute_sweeps__init__(soc):
     qubits = [Qubit()]
 
     program = ExecuteSweeps(soc, config, sequence, qubits, sweepers)
-    assert isinstance(program, ExecuteSweeps)
+    return program
+
+
+def test_execute_sweeps_init(execute_sweeps):
+    assert isinstance(execute_sweeps, ExecuteSweeps)
+
+
+def test_execute_pulsesequence_init(execute_pulse_sequence):
+    assert isinstance(execute_pulse_sequence, ExecutePulseSequence)
+
+
+def test_declare_nqz_zones(execute_pulse_sequence):
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse0",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse1",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+    ]
+    execute_pulse_sequence.declare_nqz_zones(sequence)
+
+
+def test_declare_readout_freq(execute_pulse_sequence):
+    execute_pulse_sequence.declare_readout_freq()
+
+
+def test_add_pulse_to_register(execute_pulse_sequence):
+    pulse0 = Pulse(
+        frequency=100,
+        amplitude=0.1,
+        relative_phase=0,
+        start=0,
+        duration=0.04,
+        name="pulse0",
+        type="drive",
+        dac=6,
+        adc=0,
+        shape="gaussian",
+        rel_sigma=5,
+    )
+
+    pulse1 = Pulse(
+        frequency=100,
+        amplitude=0.1,
+        relative_phase=0,
+        start=0,
+        duration=0.04,
+        name="pulse1",
+        type="drive",
+        dac=6,
+        adc=0,
+        shape="gaussian",
+        rel_sigma=5,
+        beta=0.1,
+    )
+
+    pulse2 = Pulse(
+        frequency=100,
+        amplitude=0.1,
+        relative_phase=0,
+        start=0,
+        duration=0.04,
+        name="pulse2",
+        type="drive",
+        dac=6,
+        adc=0,
+        shape="rectangular",
+    )
+
+    execute_pulse_sequence.add_pulse_to_register(pulse0)
+    execute_pulse_sequence.add_pulse_to_register(pulse1)
+    execute_pulse_sequence.add_pulse_to_register(pulse2)
+
+
+def test_body(soc):
+    config = Config()
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse0",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="gaussian",
+            rel_sigma=5,
+        ),
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse1",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="gaussian",
+            rel_sigma=5,
+        ),
+        Pulse(
+            frequency=200,
+            amplitude=0.3,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse2",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+        Pulse(
+            frequency=300,
+            amplitude=0.2,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse3",
+            type="drive",
+            dac=6,
+            adc=0,
+            shape="gaussian",
+            rel_sigma=5,
+        ),
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse4",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+    ]
+    qubits = [Qubit()]
+
+    program = ExecutePulseSequence(soc, config, sequence, qubits)
+    program.body()
+
+
+def test_set_bias(soc):
+    config = Config()
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse4",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+    ]
+    qubits = [Qubit(10, 0), Qubit(0, None), Qubit(0, 2)]
+
+    program = ExecutePulseSequence(soc, config, sequence, qubits)
+    program.set_bias("sweetspot")
+    program.set_bias("zero")
+
+
+def test_declare_nqz_flux(soc):
+    config = Config()
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse4",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+    ]
+    qubits = [Qubit(10, 0), Qubit(0, None), Qubit(0, 2)]
+
+    program = ExecutePulseSequence(soc, config, sequence, qubits)
+    program.declare_nqz_flux()
+
+
+def test_flux_body(soc):
+    config = Config()
+    sequence = [
+        Pulse(
+            frequency=100,
+            amplitude=0.1,
+            relative_phase=0,
+            start=0,
+            duration=0.04,
+            name="pulse4",
+            type="readout",
+            dac=6,
+            adc=0,
+            shape="rectangular",
+        ),
+    ]
+    qubits = [Qubit(10, 0), Qubit(0, None), Qubit(0, 2)]
+
+    program = ExecutePulseSequence(soc, config, sequence, qubits)
+    program.body()
