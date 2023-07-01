762a18ade0de8c36c093978e206ae6d522f02987
Alessandro Candido <candido.ale@gmail.com>
2023-07-01 15:03:34 +0200

    Trim configs header and comments

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,24 +1,11 @@
 """Configuration file for sphinx documentation."""
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import sys
 from pathlib import Path
 
-from sphinx.ext import apidoc
-
-sys.path.insert(0, str(Path.cwd().absolute()))
-
 import qibosoq
+from sphinx.ext import apidoc
 
 # -- Project information -----------------------------------------------------
 
