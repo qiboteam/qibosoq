3ddd4882dfece7c47f005a485c959a00dc702ea7
rodolfocarobene <rodolfo.carobene@gmail.com>
2023-08-04 15:08:22 +0400

    typo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,11 +19,6 @@ release = qibosoq.__version__
 
 # -- General configuration ---------------------------------------------------
 
-# https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
-# master_doc = "index"
-
-autodoc_mock_imports = ["qm"]
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
