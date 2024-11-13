8ec4cf9da4620195ddb01da56d1bf55e5e122522
Álvaro Orgaz <55031026+aorgazf@users.noreply.github.com>
2024-11-13 16:22:13 +0400

    change TIDAC spi mode

--- src/qibosoq/drivers/TI_DAC80508.py
+++ src/qibosoq/drivers/TI_DAC80508.py
@@ -8,7 +8,7 @@ class DAC80508:
     def __init__(self):
         self.spi = spidev.SpiDev()
         self.spi.open(1, 0)
-        self.spi.mode = 0
+        self.spi.mode = 0b01
         self.spi.max_speed_hz = 500000
 
         to_send = [0x04, 0x00, 0xff] # All gains set x2
