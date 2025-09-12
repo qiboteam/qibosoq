cb8017eb93748f8754b9c1cce024c52d9423fd4e
Alessandro Candido <candido.ale@gmail.com>
2025-09-12 11:00:31 +0200

    feat: Add pth file installation in environment

--- dev-install.bash
+++ dev-install.bash
@@ -1,11 +1,24 @@
-prog=$(
+find_deps=$(
 	cat <<'EOF'
 		/^\]/ { if (s) e=1 }
 		s && !e { match($0, /".*"/); print substr($0, RSTART+1, RLENGTH-2) }
 		/^dependencies/ { s=1 }
 EOF
 )
-awk "$prog" pyproject.toml >qibosoq-requirements.txt
+awk "$find_deps" pyproject.toml >qibosoq-requirements.txt
 
 pip install -r qibosoq-requirements.txt
 rm qibosoq-requirements.txt
+
+find_path=$(
+	cat <<-'EOF'
+		import sys
+
+		print([x for x in sys.path if "site-packages" in x][-1])
+	EOF
+)
+env_path=$(python3 -c "$find_path")
+project_dir=$(dirname "$0")
+package_path=$(realpath "$project_dir")/src
+
+echo "$package_path" >"$env_path/qibosoq.pth"
